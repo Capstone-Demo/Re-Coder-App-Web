d1a1320b1da8b909e551d32c0d022eee65afb184
khg339 <khg33333339@gmail.com>
2022-07-14 01:29:52 +0900

    푸시알림

--- app/src/main/java/com/example/basiccode/MainPage.java
+++ app/src/main/java/com/example/basiccode/MainPage.java
@@ -1,11 +1,24 @@
 package com.example.basiccode;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.installations.FirebaseInstallations;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainPage extends AppCompatActivity {
 
@@ -17,6 +30,29 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.firstpage);
+
+        //푸시알림
+        try {
+            FirebaseMessaging.getInstance().getToken()
+                    .addOnCompleteListener(new OnCompleteListener<String>() {
+                        @Override
+                        public void onComplete(@NonNull Task<String> task) {
+                            if (!task.isSuccessful()) {
+                                Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                                return;
+                            }
+
+                            // Get new FCM registration token
+                            String token = task.getResult();
+
+                            // Log and toast
+                            String msg = getString(R.string.msg_token_fmt, token);
+                            Log.d("IDService", msg);
+                        }
+                    });
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
 
         visitbutton=findViewById(R.id.visitbutton);
         managerbutton=findViewById(R.id.managerbutton);
